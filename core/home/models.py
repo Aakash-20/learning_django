e15f02beef08e38c10c5b5835615ae0aa6717aae
Akash <skyakash480@gmail.com>
2024-10-01 17:57:31 +0530

    crud operations in shell added crud.txt file

--- core/home/models.py
+++ core/home/models.py
@@ -7,5 +7,9 @@ class Student(models.Model):
     address = models.TextField(null = True, blank=True)
 
 
-class Product(models.Model):
-    pass
+class Car(models.Model):
+    car_name = models.CharField(max_length=300)
+    speed = models.IntegerField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.car_name
